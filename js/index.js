549d99215854c9eecc4e3e84dde2602298bd1898
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-05-26 14:16:25 -0700

    trying some new util classes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
 
+function checkEmail(input) {
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const errorMessage = email.nextElementSibling;
+	if (!re.test(input.value.trim())) {
+		errorMessage.classList.add('error');
+		setTimeout(() => {
+			errorMessage.classList.remove('error');
+		}, 3000);
+	}
+}
+
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+	console.log('hello from event listener');
+	checkEmail(email);
+});
